d406be6e4fb429012ab5d0c94ee9e78ea23f1492
MishaHyper <120516496+MishaHyper@users.noreply.github.com>
2023-01-13 00:49:33 +0200

    Update script.js

--- Lab 4/script.js
+++ Lab 4/script.js
@@ -56,5 +56,35 @@ function loadCatalogs(categoryShortName, link) {
         });
       }
     })
+ 
+ function loadRandom(categoryShortName, link) {
+    $(".category").hide()
+    $(".catalog").hide()
+    $.ajax({
+      url: link,
+      success: function(result) {
+        $.each(result.catalog_items, function(index, item) {
+          var catalogs = $(".catalog:first").clone()
+          var id = item.id;
+          var shortName = item.short_name;
+          var name = item.name;
+          var description = item.description;
+          var price_retail = item.price_retail;
+          var price_wholesale = item.price_wholesale;
+          var amount_retail = item.amount_retail;
+          var amount_wholesale = item.amount_wholesale;
+          $(catalogs).find(".catalog-short_name").html(shortName);
+          $(catalogs).find(".catalog-name").html(name);
+          $(catalogs).find(".catalog-description").html(description);
+          $(catalogs).find(".catalog-price_retail").html(price_retail);
+          $(catalogs).find(".catalog-price_wholesale").html(price_wholesale);
+          $(catalogs).find(".catalog-amount_retail").html(amount_retail);
+          $(catalogs).find(".catalog-amount_wholesale").html(amount_wholesale);
+          $(catalogs).find(".catalog-img").attr("src", "images/catalog/" + categoryShortName + "/" + shortName + ".jpg");
+          $(catalogs).show()
+          $(catalogs).appendTo($(".container"))
+        });
+      }
+    })
     
 };
